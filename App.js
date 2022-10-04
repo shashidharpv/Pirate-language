2b16609459451c9923bb81697e0def5af76bf847
Kumari-Smitha <pvshashi.dev@gmail.com>
2022-10-05 02:05:08 +0530

    initial commit

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ var outputDiv = document.querySelector("#output");
 
 var serverURL = "https://api.funtranslations.com/translate/pirate.json";
 
-//Take paramter and generate URL with parameter
 function getTranslationURL(text) {
   return serverURL + "?" + "text=" + text;
 }
@@ -18,18 +17,17 @@ function errorHandler(error) {
 btnTranslate.addEventListener("click", clickHandler);
 btnReset.addEventListener("click", resetItems);
 
-//Call Function when Translate button is clicked    
+ 
 function clickHandler() {
-  // outputDiv.innerText = "translated blabla: "+txtInput.value;
+
 
   var inputTxt = txtInput.value;
 
-  //Calling Server for processing
   fetch(getTranslationURL(inputTxt))
     .then((response) => response.json())
     .then((json) => {
       var translatedText = json.contents.translated;
-      outputDiv.innerText = translatedText; //Output
+      outputDiv.innerText = translatedText; 
     })
     .catch(errorHandler);
 }
